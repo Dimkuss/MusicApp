a2a39ac604d151a079701d073327ace6f6340b68
DmitryMokhov <mohow-95@mail.ru>
2022-02-12 02:08:37 +0300

    fragments

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -4,6 +4,11 @@ import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,9 +17,16 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
 
         val service = Retrofit.Builder()
@@ -31,12 +43,22 @@ class MainActivity : AppCompatActivity() {
                 t.printStackTrace()
             }
 
-            override fun onResponse(call: Call<ArtistBioResponse>, response: Response<ArtistBioResponse>) {
+            override fun onResponse(
+                call: Call<ArtistBioResponse>,
+                response: Response<ArtistBioResponse>
+            ) {
                 Log.d("TAG_", response.body().toString())
             }
         })
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
 }
+
 
 class ArtistBioResponse(val results: ArtistDataResponse)
 
@@ -44,8 +66,12 @@ class ArtistDataResponse(val artist: List<ArtistInfoResponse>)
 
 class ArtistInfoResponse(val data: ArtistInfoDataResponse)
 
-class ArtistInfoDataResponse(val name: String, image: Image, val listeners: String, val playcount: String)
-
+class ArtistInfoDataResponse(
+    val name: String,
+    image: Image,
+    val listeners: String,
+    val playcount: String
+)
 
 
 interface UserService {
